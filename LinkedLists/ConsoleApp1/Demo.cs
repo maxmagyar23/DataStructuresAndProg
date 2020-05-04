9a040ecc3638b23210ae0ef85b44b66a22b3b2bf
Max Magyar <47374376+maxmagyar23@users.noreply.github.com>
2020-05-04 14:43:44 -0500

    Add files via upload

---
+++ LinkedLists/ConsoleApp1/Demo.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    class Demo
+    {
+        static void Main(string [] args)
+        {
+
+            int choice, data, k, x;
+
+            SingleLinkedList list = new SingleLinkedList();
+
+            list.CreateList();
+
+            while (true)
+            {
+                Console.WriteLine("1. Display List");
+                Console.WriteLine("2. Count the number of nodes");
+                Console.WriteLine("3. Search for an element");
+                Console.WriteLine("4. Insert in empty/Insert in beginning oof the list");
+                Console.WriteLine("5. Insert a node at the end of the lsit");
+                Console.WriteLine("6. Insert a node after a specific node");
+                Console.WriteLine("7. Insert a node before a specific node");
+                Console.WriteLine("8. Insert a node at a given position");
+                Console.WriteLine("9. Delete first node");
+                Console.WriteLine("10. Delete last node");
+                Console.WriteLine("11. Delete any node");
+                Console.WriteLine("12. Reverse the list");
+                Console.WriteLine("13. Bubble sort by exchanging data");
+                Console.WriteLine("14. Bubble sort by exchanging links");
+                Console.WriteLine("15. MergeSort");
+                Console.WriteLine("16. Insert cycle");
+                Console.WriteLine("17. Detect Cycle");
+                Console.WriteLine("18. Remove Cycle");
+                Console.WriteLine("19. Quit");
+
+                Console.WriteLine("Enter Your Choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 19)
+                    break;
+
+                switch(choice)
+                {
+                    case 1:
+                        list.DisplayList();
+                        break;
+                    case 2:
+                        list.CountNodes();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the element to be searched: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.Search(data);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the elementto be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertInBeginning(data);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter the element to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtEnd(data);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter the element to ber inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the element after which to insert: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAfter(data, x);
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter the element to be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the element before which to be inserted: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        list.InsertBefore(data, x);
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter the element to be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the position at which to insert: ");
+                        k = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtPosition(data, k);
+                        break;
+                    case 9:
+                        list.DeleteFirstNode();
+                        break;
+                    case 10:
+                        list.DeleteLastNode();
+                        break;
+                    case 11:
+                        Console.WriteLine("Enter theelement to be deleted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.DeleteNode(data);
+                        break;
+                    case 12:
+                        list.ReverseList();
+                        break;
+                    case 13:
+                        list.BubbleSortExData();
+                        break;
+                    case 14:
+                        list.BubbleSortExLinks();
+                        break;
+                    case 16:
+                        Console.WriteLine("Enter the element which the cycle has to be inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertCycle(data);
+                        break;
+                    case 17:
+                        if (list.HasCycle())
+                            Console.WriteLine("List has a cycle");
+                        else
+                            Console.WriteLine("List does not have a cycle");
+                        break;
+                    case 18:
+                        list.RemoveCycle();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Exiting");
+        }
+    }
+}
---
+++ LinkedLists/ConsoleApp1/SingleLinkedList.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    class SingleLinkedList
+    {
+        private Node start;
+
+        public SingleLinkedList()
+        {
+            start = null;
+        }
+
+        public void DisplayList()
+        {
+            Node p;
+            if (start == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            Console.Write("List is: ");
+            p = start;
+            while (p != null)
+            {
+                Console.WriteLine(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+
+        public void CountNodes()
+        {
+            int n = 0;
+            Node p = start;
+            while (p != null)
+            {
+                n++;
+                p = p.link;
+            }
+            Console.WriteLine("Number of nodes in the list = " + n);
+        }
+
+        public bool Search(int x)
+        {
+            int position = 1;
+            Node p = start;
+            while (p != null)
+            {
+                if (p.info == x)
+                    break;
+                position++;
+                p = p.link;
+            }
+            if (p == null)
+            {
+                Console.WriteLine(x + " not found in list");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine(x + "is a position " + position);
+                return true;
+            }
+        }
+
+        public void InsertInBeginning(int data)
+        {
+            Node temp = new Node(data);
+            temp.link = start;
+            start = temp;
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node p;
+            Node temp = new Node(data);
+
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+
+            p = start;
+            while (p.link != null)
+                p = p.link;
+
+            p.link = temp;
+        }
+
+        public void CreateList()
+        {
+            int i, n, data;
+
+            Console.WriteLine("Enter the number of nodes: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter the element to be inserted : ");
+                data = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(data);
+            }
+        }
+
+        public void InsertAfter(int data, int x)
+        {
+            Node p = start;
+            while (p != null)
+            {
+                if (p.info == x)
+                    break;
+                p = p.link;
+            }
+
+            if (p == null)
+                Console.WriteLine(x + "not present in the lsit");
+            else
+            {
+                Node temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+            }
+        }
+
+        public void InsertBefore(int data, int x)
+        {
+            Node temp;
+
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            if (x == start.info)
+            {
+                temp = new Node(data);
+                temp.link = start;
+                start = temp;
+                return;
+            }
+
+            Node p = start;
+            while (p.link != null)
+            {
+                if (p.link.info == x)
+                    break;
+                p = p.link;
+            }
+
+            if (p.link == null)
+                Console.WriteLine(x + " not present in the list");
+            else
+            {
+                temp = new Node(data);
+                temp.link = p.link;
+            }
+        }
+
+        public void InsertAtPosition(int data, int k)
+        {
+            Node temp;
+            int i;
+
+            if (k == 1)
+            {
+                temp = new Node(data);
+                temp.link = start;
+                start = temp;
+                return;
+            }
+
+            Node p = start;
+            for (i = 1; i < k - 1 && p != null; i++)
+                p = p.link;
+            if (p == null)
+                Console.WriteLine("You can ony insert only up-to " + i + "th position");
+            else
+            {
+                temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+            }
+        }
+
+        public void DeleteFirstNode()
+        {
+            if (start == null)
+                return;
+            start = start.link;
+        }
+
+        public void DeleteLastNode()
+        {
+            if (start == null)
+                return;
+
+            if (start.link == null)
+            {
+                start = null;
+                return;
+            }
+
+            Node p = start;
+            while (p.link.link != null)
+                p = p.link;
+            p.link = null;
+        }
+
+        public void DeleteNode(int x)
+        {
+            if (start == null)
+            {
+                Console.WriteLine("List is empty\n");
+                return;
+            }
+
+            if (start.info == x)
+            {
+                start = start.link;
+                return;
+            }
+
+            Node p = start;
+            while (p.link != null)
+            {
+                if (p.link != null) ;
+                break;
+                p = p.link;
+            }
+
+            if (p.link == null)
+                Console.WriteLine("Element " + x + " not in list");
+            else
+                p.link = p.link.link;
+        }
+
+        public void ReverseList()
+        {
+            Node prev, p, next;
+            prev = null;
+            p = start;
+            while (p != null)
+            {
+                next = p.link;
+                p.link = prev;
+                prev = p;
+                p = next;
+            }
+            start = prev;
+        }
+
+        public void BubbleSortExData()
+        {
+            Node end, p, q;
+
+            for (end = null; end != start.link; end = p)
+            {
+                for (p = start; p.link != end; end = p.link)
+                {
+                    q = p.link;
+                    if (p.info > q.info)
+                    {
+                        int temp = p.info;
+                        p.info = q.info;
+                        q.info = temp;
+                    }
+                }
+            }
+        }
+
+        public void BubbleSortExLinks()
+        {
+            Node end, r, p, q, temp;
+
+            for (end = null; end != start.link; end = p)
+            {
+                for (r = p = start; p.link != end; r = p, p = p.link)
+                {
+                    q = p.link;
+                    if (p.info > q.info)
+                    {
+                        p.link = q.link;
+                        q.link = p;
+                        if (p != start)
+                            r.link = q;
+                        else
+                            start = q;
+                        temp = p;
+                        p = q;
+                        q = temp;
+
+                    }
+                }
+            }
+        }
+
+        public bool HasCycle()
+        {
+            if (FindCycle() == null)
+                return false;
+            else
+                return true;
+        }
+
+        private Node FindCycle()
+        {
+            if (start == null || start.link == null)
+                return null;
+
+            Node slowR = start, fastR = start;
+
+            while (fastR != null && fastR.link != null)
+            {
+                slowR = slowR.link;
+                fastR = fastR.link.link;
+                if (slowR == fastR)
+                    return slowR;
+            }
+            return null;
+        }
+
+        public void RemoveCycle()
+        {
+            Node c = FindCycle();
+            if (c == null)
+                return;
+            Console.WriteLine("Node at which the cycle was detected is " + c.info);
+
+            Node p = c, q = c;
+            int lenCycle = 0;
+            do
+            {
+                lenCycle++;
+                q = q.link;
+            } while (p != q);
+            Console.WriteLine("Length of cycle is : " + lenCycle);
+
+            int lenRemList = 0;
+            p = start; 
+            while (p != q)
+            {
+                lenRemList++;
+                p = p.link;
+                q = q.link;
+            }
+
+            Console.WriteLine("Number of nodes not included in the cycle are :" + lenCycle);
+
+            int lengthlist = lenCycle + lenRemList;
+            Console.WriteLine("Length of the list is : " + lengthlist);
+
+            p = start;
+            for (int i = 1; i <= lengthlist - 1; i++)
+                p = p.link;
+            p.link = null;
+            
+        }
+
+        public void InsertCycle(int x)
+        {
+            if (start == null)
+                return;
+
+            Node p = start, px = null, prev = null;
+
+            while (p!=null)
+            {
+                if (p.info == x)
+                    px = p;
+                prev = p;
+                p = p.link;
+            }
+            if (px != null)
+                prev.link = px;
+            else
+                Console.WriteLine(x + " not present in list");
+
+        }
+
+     
+       
+    }
+}
